b5194f0fc8f55729293c6415f89558804b5de794
Deathshamhd <122151628+Deathshamhd@users.noreply.github.com>
2023-08-28 13:37:49 +0300

    task 4 fixed

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,11 @@
-let counterValue = 0;
-
-
-document.getElementById('increment').addEventListener('click', () => {
-  
-    counterValue++;
-    document.getElementById('counter').textContent = counterValue;
-});
-
-
-document.getElementById('decrement').addEventListener('click', () => {
-    
+let  counterValue = 0;
+  document.querySelector('[data-action="decrement"]').addEventListener('click', () => {
     counterValue--;
- 
-    document.getElementById('counter').textContent = counterValue;
+    document.querySelector('#value').textContent = counterValue;
+  });
+  document.querySelector('[data-action="increment"]').addEventListener('click', () => {
+
+    counterValue++;
+
+    document.querySelector('#value').textContent = counterValue;
   });
--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,16 @@
 const form = document.querySelector('.login-form');
 
+
+// Form submission (form.login-form) must be processed on the submit event.
 form.addEventListener('submit', (event) => {
+    //The page must not reload when the form is submitted.
   event.preventDefault();
 
   const elements = form.elements;
 
   const email = elements.email.value;
   const password = elements.password.value;
-
+//If the form has empty fields, display alert saying that all fields must be filled in.
   if (!email || !password) {
     alert('All fields must be filled in');
     return;
